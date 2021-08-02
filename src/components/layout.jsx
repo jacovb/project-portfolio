4eaef65e0c0497b7744f161d4960fa6044a2a4eb
jacovb <vbiljon@gmail.com>
2021-08-02 18:42:31 +0100

    Navbar Header done

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { Link } from 'react-router-dom';
+import HomeIcon from '@material-ui/icons/Home';
 
 const Layout = ({ pageTitle, children }) => {
   return (
@@ -7,46 +8,43 @@ const Layout = ({ pageTitle, children }) => {
       <title>{pageTitle}</title>
       <div className="grid-container">
         <div className="block-1"></div>
-        <div className="block-2"></div>
-        <div className="block-3"></div>
-        <div className="block-4 row-start-3 row-end-5 col-start-1 col-end-3 bg-white"></div>
-        <div className="block-5 row-start-3 row-end-5 col-start-3 col-end-11 bg-blue-500 ">
-          <nav className="flex h-full w-full items-center justify-center">
-            <Link
-              to="/"
-              className="flex-1 bg-white border-2 border-black rounded-full py-2 ml-4 mr-2 text-black text-center hover:bg-blue-700 hover:text-white hover:border-white transition-all duration-300 ease-in-out "
-            >
-              Home
-            </Link>
-
+        <div className="block-2">
+          <nav className="main-navbar">
             <Link
               to="/about"
-              className="flex-1 bg-white border-2 border-black rounded-full py-2 mx-2 text-black text-center hover:bg-blue-700 hover:text-white hover:border-white transition-all duration-300 ease-in-out"
+              className="main-navbar-button"
             >
               About
             </Link>
 
             <Link
-              to="/page-2"
-              className="flex-1 bg-white border-2 border-black rounded-full py-2 ml-2 mr-4 text-black text-center hover:bg-blue-700 hover:text-white hover:border-white transition-all duration-300 ease-in-out"
+              to="/portfolio"
+              className="main-navbar-button"
             >
-              Page-2
+              Portfolio
+            </Link>
+
+            <Link
+              to="/contact"
+              className="main-navbar-button"
+            >
+              Contact
             </Link>
           </nav>
         </div>
-        <div className="row-start-3 row-end-5 col-start-11 col-end-19 bg-yellow-500"></div>
-        <div className="row-start-5 row-end-11 col-start-1 col-end-3 bg-white"></div>
-        <div className="row-start-5 row-end-11 col-start-3 col-end-13 bg-white">
+        <div className="block-3"></div>
+        <div className="block-4">
+          <Link
+            to="/">
+            <HomeIcon />
+          </Link>
+          
+        </div>
+        <div className="block-5"></div>
+        <div className="block-6"></div>
+      </div>
           <h1>{pageTitle}</h1>
           {children}
-        </div>
-        <div className="row-start-5 row-end-9 col-start-13 col-end-19 bg-white"></div>
-        <div className="row-start-9 row-end-11 col-start-13 col-end-19 bg-blue-500"></div>
-        <div className="row-start-11 row-end-13 col-start-1 col-end-3 bg-white"></div>
-        <div className="row-start-11 row-end-13 col-start-3 col-end-6 bg-red-500"></div>
-        <div className="row-start-11 row-end-13 col-start-6 col-end-11 bg-white"></div>
-        <div className="row-start-11 row-end-13 col-start-11 col-end-19 bg-white"></div>
-      </div>
     </main>
   )
 }
