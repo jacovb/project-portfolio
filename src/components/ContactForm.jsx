1fe2eba329b6aa5d0334f2f901dac8f43d83a80e
jacovb <vbiljon@gmail.com>
2021-08-15 19:21:53 +0100

    css figuring out stuff

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -7,18 +7,18 @@ export default function ContactForm() {
       <div className="contact-form">
         <div>
           <input type="text" id="name" name="name" required />
-          <label htmlFor="name">Name</label>
+          <label htmlFor="name">NAME</label>
         </div>
         <div>
           <input type="email" id="email" name="email" required />
-          <label htmlFor="email">Email</label>
+          <label htmlFor="email">EMAIL</label>
         </div>
       </div>
       
       <div className="contact-form">
         <div>
           <textarea id="message" name="message" required></textarea>
-          <label htmlFor="message">Message</label>
+          <label htmlFor="message">MESSAGE</label>
         </div>
       </div>
       <p>
