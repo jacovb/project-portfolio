595d0a57be91d51d49eabb95daa815d30a1a0e75
jacovb <vbiljon@gmail.com>
2021-08-18 06:48:50 +0100

    simplify contact form css

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -5,17 +5,19 @@ export default function ContactForm() {
     <form name="contact" method="post" className="contact-form"> 
       <input type="hidden" name="form-name" value="contact" />
       <div>
-        <input type="text" id="name" name="name" required />
-        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="name" placeholder="Name" className="input-name" required />
+        {/* <label htmlFor="name">Name</label> */}
       </div>
       <div>
-        <input type="email" id="email" name="email" required />
-        <label htmlFor="email">Email</label>
+        <input type="email" id="email" name="email" placeholder="Email" className="input-email" required />
+        {/* <label htmlFor="email">Email</label> */}
       </div>
+      <div className="break"></div>
       <div>
-        <textarea id="message" name="message" required></textarea>
-        <label htmlFor="message">Message</label>
+        <textarea id="message" name="message" placeholder="Message" required></textarea>
+        {/* <label htmlFor="message">Message</label> */}
       </div>
+      <div className="break"></div>
       <p>
         <input type="submit" value="Send" />
       </p>
