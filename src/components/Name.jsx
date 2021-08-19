9a0faf7b57f5b622477a602fdb4d16a1c8c91e7b
jacovb <vbiljon@gmail.com>
2021-08-19 18:12:39 +0100

    css tweak

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -4,9 +4,9 @@ import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 export default function Name() {
   return (
-    <div>
+    <div className="name-component">
       <h1 style={{margin: "0 auto 0 auto"}}>JACO VAN BILJON</h1>
-      <div style={{display: "flex", justifyContent: "space-between", width: "380px", margin: "auto"}}>
+      <div style={{display: "flex", justifyContent: "space-between", width: "380px", margin: "0 auto 0 auto"}}>
         <h4><FontAwesomeIcon icon={faMapMarkerAlt} /> LONDON, UK</h4>
         <h4>FRONT-END DEVELOPER</h4>
       </div>
