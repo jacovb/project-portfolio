06f56ee77964233b8413032dfd9c9ae0976b48df
jacovb <vbiljon@gmail.com>
2021-08-19 08:07:55 +0100

    contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 export default function ContactForm() {
   return (
-    <form name="contact" method="post" className="contact-form"> 
-      <input type="hidden" name="form-name" value="contact" />
-      <div className="contact-id">
-        <input type="text" id="name" name="name" placeholder="Name" className="input-name" required />
-        {/* <label htmlFor="name">Name</label> */}
+    <>
+      <h1>Contact</h1>
+      <div className="contact-header-mail">
+        <FontAwesomeIcon icon={faEnvelope} />
+        <div>vbiljon@gmail.com</div>
+      </div>
 
-        <input type="email" id="email" name="email" placeholder="Email" className="input-email" required />
-        {/* <label htmlFor="email">Email</label> */}
+      <div className="contact-header-mail">
+        <FontAwesomeIcon icon={faPhone} />
+        <div>0747 007 4039</div>
       </div>
-      <div>
+   
+      <form name="contact" method="post" className="contact-form"> 
+        <input type="hidden" name="form-name" value="contact" />
+        <div className="contact-id">
+          <input type="text" id="name" name="name" placeholder="Name" className="input-name" required />
+          <input type="email" id="email" name="email" placeholder="Email" className="input-email" required />
+        </div>
         <textarea id="message" name="message" placeholder="Message" required></textarea>
-        {/* <label htmlFor="message">Message</label> */}
-      </div>
-      <input type="submit" value="Send" className="contact-send-button"/>
-    </form>
+        <input type="submit" value="Send" className="contact-send-button"/>
+      </form>
+    </>
   );
 }
