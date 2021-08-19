06cc30efa55cf9fcabac30bbb2cbbbdebd13a214
jacovb <vbiljon@gmail.com>
2021-08-19 06:54:10 +0100

    contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -4,23 +4,18 @@ export default function ContactForm() {
   return (
     <form name="contact" method="post" className="contact-form"> 
       <input type="hidden" name="form-name" value="contact" />
-      <div>
+      <div className="contact-id">
         <input type="text" id="name" name="name" placeholder="Name" className="input-name" required />
         {/* <label htmlFor="name">Name</label> */}
-      </div>
-      <div>
+
         <input type="email" id="email" name="email" placeholder="Email" className="input-email" required />
         {/* <label htmlFor="email">Email</label> */}
       </div>
-      <div className="break"></div>
       <div>
         <textarea id="message" name="message" placeholder="Message" required></textarea>
         {/* <label htmlFor="message">Message</label> */}
       </div>
-      <div className="break"></div>
-      <p>
-        <input type="submit" value="Send" />
-      </p>
+      <input type="submit" value="Send" className="contact-send-button"/>
     </form>
   );
 }
