e7b5725fec00626dd7f85fc8295a86d3348f710a
jacovb <vbiljon@gmail.com>
2021-09-03 19:24:48 +0100

    portfolio section scroll rethink

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,87 +12,90 @@ import StarWarsApp from '../images/StarWarsApp.gif'
 
 export default function Portfolio() {
   return (
-    <div className="project-slider">
-      <ProjectIndex />
-      <Project 
-        name="Punch-Card" 
-        gifLink={PunchCard}
-        description="Employees Add, Edit and Manage 
-            timesheet entries and automatically creates reports that can be used by employers for 
-            business and resource management"
-        demoLink="https://master.d13ledzsxl0qxj.amplifyapp.com/"
-        codeLink="https://github.com/jacovb/time-harvest"
-        projectStyle="punch-card-styles"
-        next="#calculator-styles"
-      >
-        <Tooltip title="HTML">
-          <FontAwesomeIcon icon={faHtml5} style={{color: "#E44D26"}} />
-        </Tooltip>
-        <Tooltip title="CSS">
-          <FontAwesomeIcon icon={faCss3} style={{color: "#2965f1"}}/>
-        </Tooltip>
-        <Tooltip title="Javascript">
-          <FontAwesomeIcon icon={faJs} style={{color: "#F0DB4F"}}/>
-        </Tooltip>
-        <Tooltip title="React">
-          <FontAwesomeIcon icon={faReact} style={{color: "#61DBFB"}}/>
-        </Tooltip>
-        <Tooltip title="AWS">
-          <FontAwesomeIcon icon={faAws} style={{color: "#F05340"}}/>
-        </Tooltip>
-        <Tooltip title="GraphQL">
-          <img src="https://upload.wikimedia.org/wikipedia/commons/1/17/GraphQL_Logo.svg" alt="graphql" height="35px" />
-        </Tooltip>
-      </Project>
+    <div className="portfolio-container">
+      <div className="project-slider">
+        <ProjectIndex />
+        <Project 
+          name="Punch-Card" 
+          gifLink={PunchCard}
+          description="Employees Add, Edit and Manage 
+              timesheet entries and automatically creates reports that can be used by employers for 
+              business and resource management"
+          demoLink="https://master.d13ledzsxl0qxj.amplifyapp.com/"
+          codeLink="https://github.com/jacovb/time-harvest"
+          projectStyle="punch-card-styles"
+          next="#calculator-styles"
+        >
+          <Tooltip title="HTML">
+            <FontAwesomeIcon icon={faHtml5} style={{color: "#E44D26"}} />
+          </Tooltip>
+          <Tooltip title="CSS">
+            <FontAwesomeIcon icon={faCss3} style={{color: "#2965f1"}}/>
+          </Tooltip>
+          <Tooltip title="Javascript">
+            <FontAwesomeIcon icon={faJs} style={{color: "#F0DB4F"}}/>
+          </Tooltip>
+          <Tooltip title="React">
+            <FontAwesomeIcon icon={faReact} style={{color: "#61DBFB"}}/>
+          </Tooltip>
+          <Tooltip title="AWS">
+            <FontAwesomeIcon icon={faAws} style={{color: "#F05340"}}/>
+          </Tooltip>
+          <Tooltip title="GraphQL">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/17/GraphQL_Logo.svg" alt="graphql" height="35px" />
+          </Tooltip>
+        </Project>
 
-      <Project
-        name="Mortgage Payment Calculator"
-        gifLink={PaymentCalculator}
-        description="Calculates and vizualises Mortgage Payments. 
-            See how Interest Rates and Initial Fixed Rate Periods impact your 
-            Oustanding Capital Amount"
-        demoLink="https://payment-calculator120.netlify.app/"
-        codeLink="https://github.com/jacovb/payment-calculator"
-        projectStyle="calculator-styles"
-        next="#star-wars-styles"
-      >
-        <Tooltip title="HTML">
-          <FontAwesomeIcon icon={faHtml5} style={{color: "#E44D26"}} />
-        </Tooltip>
-        <Tooltip title="CSS">
-          <FontAwesomeIcon icon={faCss3} style={{color: "#2965f1"}}/>
-        </Tooltip>
-        <Tooltip title="Javascript">
-          <FontAwesomeIcon icon={faJs} style={{color: "#F0DB4F"}}/>
-        </Tooltip>
-        <Tooltip title="React">
-          <FontAwesomeIcon icon={faReact} style={{color: "#61DBFB"}}/>
-        </Tooltip>
-      </Project>
+        <Project
+          name="Mortgage Payment Calculator"
+          gifLink={PaymentCalculator}
+          description="Calculates and vizualises Mortgage Payments. 
+              See how Interest Rates and Initial Fixed Rate Periods impact your 
+              Oustanding Capital Amount"
+          demoLink="https://payment-calculator120.netlify.app/"
+          codeLink="https://github.com/jacovb/payment-calculator"
+          projectStyle="calculator-styles"
+          next="#star-wars-styles"
+        >
+          <Tooltip title="HTML">
+            <FontAwesomeIcon icon={faHtml5} style={{color: "#E44D26"}} />
+          </Tooltip>
+          <Tooltip title="CSS">
+            <FontAwesomeIcon icon={faCss3} style={{color: "#2965f1"}}/>
+          </Tooltip>
+          <Tooltip title="Javascript">
+            <FontAwesomeIcon icon={faJs} style={{color: "#F0DB4F"}}/>
+          </Tooltip>
+          <Tooltip title="React">
+            <FontAwesomeIcon icon={faReact} style={{color: "#61DBFB"}}/>
+          </Tooltip>
+        </Project>
 
-      <Project
-        name="Star Wars App"
-        gifLink={StarWarsApp}
-        description="My introduction to API requests, React Hooks and React Router, 
-            mixed with a bit of fun styling"
-        demoLink="https://star-wars-app200.netlify.app/"
-        codeLink="https://github.com/jacovb/star-wars-app"
-        projectStyle="star-wars-styles"
-        next=""
-      >
-        <Tooltip title="HTML">
-          <FontAwesomeIcon icon={faHtml5} style={{color: "#E44D26"}} />
-        </Tooltip>
-        <Tooltip title="CSS">
-          <FontAwesomeIcon icon={faCss3} style={{color: "#2965f1"}}/>
-        </Tooltip>
-        <Tooltip title="Javascript">
-          <FontAwesomeIcon icon={faJs} style={{color: "#F0DB4F"}}/>
-        </Tooltip>
-        <Tooltip title="React">
-          <FontAwesomeIcon icon={faReact} style={{color: "#61DBFB"}}/>
-        </Tooltip>
-      </Project>
+        <Project
+          name="Star Wars App"
+          gifLink={StarWarsApp}
+          description="My introduction to API requests, React Hooks and React Router, 
+              mixed with a bit of fun styling"
+          demoLink="https://star-wars-app200.netlify.app/"
+          codeLink="https://github.com/jacovb/star-wars-app"
+          projectStyle="star-wars-styles"
+          next=""
+        >
+          <Tooltip title="HTML">
+            <FontAwesomeIcon icon={faHtml5} style={{color: "#E44D26"}} />
+          </Tooltip>
+          <Tooltip title="CSS">
+            <FontAwesomeIcon icon={faCss3} style={{color: "#2965f1"}}/>
+          </Tooltip>
+          <Tooltip title="Javascript">
+            <FontAwesomeIcon icon={faJs} style={{color: "#F0DB4F"}}/>
+          </Tooltip>
+          <Tooltip title="React">
+            <FontAwesomeIcon icon={faReact} style={{color: "#61DBFB"}}/>
+          </Tooltip>
+        </Project>
+      </div>
+      <div className="portfolio-scroll-bar"></div>
     </div>
   )
 }
