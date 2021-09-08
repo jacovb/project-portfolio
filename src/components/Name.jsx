b6fbbeab4bde44b8e22023c95a83b5048db8873d
jacovb <vbiljon@gmail.com>
2021-09-08 20:49:39 +0100

    home page animations

--- src/components/Name.jsx
+++ src/components/Name.jsx
@@ -5,11 +5,16 @@ import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 export default function Name() {
   return (
     <div className="name-component">
-      <h1 style={{margin: "0 auto 0 auto"}}>JACO VAN BILJON</h1>
-      <div style={{display: "flex", justifyContent: "space-between", width: "380px", margin: "0 auto 0 auto"}}>
-        <h4><FontAwesomeIcon icon={faMapMarkerAlt} /> LONDON, UK</h4>
-        <h4>FRONT-END DEVELOPER</h4>
+      <div className="home-text">
+        <div className="home-line topline"></div>
+        <h1>JACO VAN BILJON</h1>
+        <div className="home-text-sub">
+          <h4><FontAwesomeIcon icon={faMapMarkerAlt} /> LONDON, UK</h4>
+          <h4>FRONT-END DEVELOPER</h4>
+        </div>
+        <div className="home-line bottomline"></div>
       </div>
+      <div className="name-background-fade"></div>
     </div>
   )
 }
