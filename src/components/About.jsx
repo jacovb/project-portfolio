cf3cbe46b4f60562e011a3910c2e113f700db2cf
jacovb <vbiljon@gmail.com>
2021-09-13 20:31:22 +0100

    condensing text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,6 +8,17 @@ export default function About() {
         <h1>
           From Architecture to Software Development
         </h1>
+
+        <p>
+          Disinterested in my career prospects in the construction industry, 
+          I tried coding to see if I liked it. It quickly grew into a habit 
+          of coding every day – I got hooked on learning, and I like it!
+        </p>
+        <p>
+          The more I learn, the more there is to learn, but my pace of learning 
+          could vastly improve if I was working in the field. What attracts me to 
+          Software Development is the opportunity to continuously learn and grow.
+        </p>
         
         <h4>
           With my background in Design and Architecture, these are my top 3 transferable 
@@ -17,67 +28,21 @@ export default function About() {
         <h2>
           I like to solve problems
         </h2>
-        <p>
-          Designing buildings and spaces is, essentially, creative problem solving: 
-          <ul>
-            <li>
-              How to improve the layout of a room to enhance the function of the building / space?
-            </li>
-            <li>
-              How can the building envelope be arranged / shaped to improve the light quality and building climate inside?
-            </li>
-            <li>
-              Ultimately, how can the User Experience be improved or enhanced?
-            </li>
-          </ul>
-        </p>
-        <p>
-          During the construction phase, problems pop up daily, which requires quick 
-          thinking for alternative solutions that would reach the same goal.
-        </p>
-        <p>
-          I believe this way of thinking and strategising is an asset for a Software Developer  
-          by solving User Problems, simplifying a service or process and creating an improved 
-          User Experience.
-        </p>
 
         <h2>
           I like to learn
         </h2>
-        <p>
-          Disinterested in my career prospects in the construction industry, 
-          I tried coding to see if I liked it. It quickly grew into a habit 
-          of coding every day – I got hooked on learning, and I like it!
-        </p>
-        <p>
-          After doing online courses through FreeCodeCamp, Scrimba, Udemy, 
-          YouTube and University of Google the real learning leap came when 
-          I started building my own projects using HTML, CSS, JavaScript 
-          with React. When I needed a Database and Authorization for an app, 
-          I learned how to use AWS Amplify, Cognito and DynamoDB and GraphQL 
-          to access the information.
-        </p>
-        <p>
-          The more I learn, the more there is to learn, but the pace of learning 
-          would vastly improve if I was working in the field. What attracts me to 
-          Software Development is the opportunity to continuously learn and grow.
-        </p>
 
         <h2>
           I like to build things
         </h2>
+
         <p>
-          This is the overarching theme of my career path so far. Starting as an 
-          architect, then shifted more to the nuts and bolts of it, as an 
-          Architectural Technician, add Project Management and Coordination and 
-          that’s where I am today. It's incredibly satisfying to create something 
-          tangible through hard work. 
+          These skills are manifested and enhanced with Teamwork, Creativity, 
+          Time-management and Good Communication - all valuable skills that 
+          I bring to the table as a Front-End Developer.
         </p>
-        <p>
-          Teamwork, creativity, time-management and good communication are all 
-          skills needed to build something - these are valuable skills that I can 
-          offer as a Front-End Developer.
-        </p>
+        
       </div>
     </>
   )
